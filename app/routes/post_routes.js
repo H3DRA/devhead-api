4eded2082d1fcf555d16d6ea048acd13090f39dc
mdelgado509 <mdelgado509@gmail.com>
2021-05-10 12:09:59 -0400

    Add update route and test endpoint

--- app/routes/post_routes.js
+++ app/routes/post_routes.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const passport = require('passport')
 const Post = require('../models/post')
-// const customErrors = require('../../lib/custom_errors')
-// const handle404 = customErrors.handle404
-// const requireOwnership = customErrors.requireOwnership
-// const removeBlanks = require('../../lib/remove_blank_fields')
+const customErrors = require('../../lib/custom_errors')
+const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const removeBlanks = require('../../lib/remove_blank_fields')
 const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
+// CREATE
+// POST /posts
 router.post('/posts', requireToken, (req, res, next) => {
   req.body.post.owner = req.user.id
   Post.create(req.body.post)
@@ -25,6 +27,29 @@ router.get('/posts', requireToken, (req, res, next) => {
       return posts.map(post => post.toObject())
     })
     .then(posts => res.status(200).json({ posts: posts }))
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /posts/6099578461dd6be72ba96d87
+router.patch('/posts/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.post.owner
+
+  Post.findById(req.params.id)
+    .then(handle404)
+    .then(post => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, post)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return post.updateOne(req.body.post)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
